6f498a2f34ba4021c63ab2c6eb8d59cc3815fd47
Eugene Janusov <esycat@gmail.com>
2025-01-29 15:33:34 +1100

    [ENG-22] Minor aesthetic improvements

--- src/lib/wizard/steps/areas-select.svelte
+++ src/lib/wizard/steps/areas-select.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <article class="prose">
-    <h1>Select your Life Areas</h1>
+    <h1>Select Your Life Areas</h1>
     <section>
         <p>After your previous reflection, let’s now select your own areas of life.</p>
         <p>Below are some examples of the most common areas of life people choose from. Most people usually pick between 4 to 8 areas of
@@ -15,10 +15,10 @@
 
         <ul>
             {#each areas as area, i}
-                <li>
+                <li class="p-1">
                     <input type="checkbox" name="selected-areas" bind:group={$selectedAreas} value="{area.uid}" id="area-{i}" />
                     <label for="area-{i}" class="font-bold">{area.name}</label>
-                    <div>{area.description}</div>
+                    <p class="my-1">{area.description}</p>
                 </li>
             {/each}
         </ul>
