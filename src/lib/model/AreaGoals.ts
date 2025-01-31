6c56b5880a4d49408bb1df240f505c15da95d44c
Eugene Janusov <esycat@gmail.com>
2025-01-31 18:59:01 +1100

    [ENG-27] Define `AreaGoals` interface

---
+++ src/lib/model/AreaGoals.ts
@@ -0,0 +1,12 @@
+export default interface AreaGoals {
+    areaUid: string;
+    assessment?: number;
+    vision?: string;
+    purpose?: string;
+    annual?: string;
+    q1?: string;
+    q2?: string;
+    q3?: string;
+    q4?: string;
+    gauge?: string;
+}
