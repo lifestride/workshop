1ecbc70bfd6065a096d0b08e0ec979d83fd99ce9
Eugene Janusov <esycat@gmail.com>
2025-02-04 00:20:08 +1100

    [ENG-29] Add print page and styling

--- src/lib/wizard/Navigation.svelte
+++ src/lib/wizard/Navigation.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
+
     let { steps, currentStep, navigate } = $props();
 
     const isAtFirstStep = () => ($currentStep === 1);
@@ -6,6 +8,8 @@
 
     const previous = () => navigate($currentStep - 1);
     const next = () => navigate($currentStep + 1);
+
+    const print = () => goto("./print");
 </script>
 
 <nav>
@@ -13,7 +17,9 @@
         <button type="button" class="back" onclick={previous}>Back</button>
     {/if}
 
-    {#if !isAtLastStep()}
+    {#if isAtLastStep()}
+        <button type="button" class="print" onclick={print}>Print</button>
+    {:else}
         <button type="button" class="next" onclick={next}>Next</button>
     {/if}
 </nav>
---
+++ src/routes/print/+page.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+    import { areas }           from "$lib/data/areas";
+    import type Area           from "$lib/model/Area";
+    import type AreaGoals      from "$lib/model/AreaGoals";
+    import { persistentStore } from "$lib/persistentStore";
+    import { onMount }         from "svelte";
+
+    const groupedAreas = new Map<string, Area>(areas.map(area => [area.uid, area]));
+
+    const reflection = persistentStore<string>("reflection", "");
+    const selectedAreas = persistentStore<string[]>("selected-areas", []);
+    const goals = persistentStore<Array<AreaGoals>>("goals", []);
+
+    const empty = "empty :-(";
+
+    onMount(() => {
+        setTimeout(() => {
+            window.print();
+        }, 1000);
+    });
+</script>
+
+<style>
+    article section + section {
+        margin: 2rem 0;
+    }
+</style>
+
+<article class="prose">
+    <h1>Your Summary</h1>
+    {@html $reflection}
+    {#each $goals as goal}
+        {#if $selectedAreas.includes(goal.areaUid)}
+            {@const area = groupedAreas.get(goal.areaUid)!}
+            <section>
+                <h2>{area.name}</h2>
+
+                <p>Self-assessment: {goal.assessment}</p>
+
+                <section>
+                    <h3>Vision</h3>
+                    {@html goal.vision ?? empty}
+                </section>
+
+                <section>
+                    <h3>Big Why</h3>
+                    {@html goal.purpose ?? empty}
+                </section>
+
+                <section>
+                    <h3>Annual Goals</h3>
+                    {@html goal.annual ?? empty}
+                </section>
+
+                <section>
+                    <h3>Q1 Milestones</h3>
+                    {@html goal.q1 ?? empty}
+                </section>
+
+                <section>
+                    <h3>Q2 Milestones</h3>
+                    {@html goal.q2 ?? empty}
+                </section>
+
+                <section>
+                    <h3>Q2 Milestones</h3>
+                    {@html goal.q2 ?? empty}
+                </section>
+
+                <section>
+                    <h3>Q2 Milestones</h3>
+                    {@html goal.q2 ?? empty}
+                </section>
+
+                <section>
+                    <h3>Gauge for Success</h3>
+                    {@html goal.gauge ?? empty}
+                </section>
+            </section>
+        {/if}
+    {/each}
+</article>
