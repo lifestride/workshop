0b1ff7c92261db00acf45c96bcb0d7d02e318723
Eugene Janusov <esycat@gmail.com>
2025-02-13 23:08:34 +1100

    [ENG-36] Update content

--- src/lib/wizard/steps/getting-started.svelte
+++ src/lib/wizard/steps/getting-started.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+    import intros              from "$lib/data/intros.json";
     import type User           from "$lib/model/User";
     import { persistentStore } from "$lib/persistentStore";
-    import intros              from "$lib/data/intros.json";
 
     const user = persistentStore<User>("user", { firstName: "" });
 
@@ -9,31 +9,44 @@
 </script>
 
 <article class="prose">
-    <h1>Getting Started!</h1>
-
     <section>
-        <ol>
-            <li>A brief introduction and explanation of what it all is about.</li>
-            <li>
-                <span>A brief and user-friendly explanation of:</span>
-                <ol>
-                    <li>Data privacy — we cannot retain or access the data.</li>
-                    <li>Limitations — the same browser on the same computer must be used.</li>
-                    <li>Data safety — data can be lost on the user side.</li>
-                    <li>
-                        <span>Recommendations:</span>
-                        <ul>
-                            <li>Use a proper large-screen computer to complete the form.</li>
-                            <li>Download the PDF on the last step to preserve the results.</li>
-                        </ul>
-                    </li>
-                </ol>
-            </li>
-        </ol>
+        <p>Hi everyone.</p>
+        <p>We are so excited you are taking the time to review this with us.</p>
+        <p>
+            The outcome of this portion of the product, is to help someone who has never set goals before,
+            to be able to create areas of their life, a vision, a gauge for success, and achievable goals for each area of their life.
+            The feedback we are looking for is on the actual “content” you are reading, as well as the prompt questions for each area.
+        </p>
+        <p>Are the questions helpful? Do you find it easy to create an area of focus for your life?</p>
+        <p>During your goal-setting process, is there anything else we should include that we may have overlooked?</p>
 
         <div class="flex items-center justify-center w-3/5 h-80 mx-auto my-10 border-2 border-neutral-300 rounded-lg text-center">
             <p>A video from Gary with introduction and guidance.<br />(use <a href="https://loom.com/">Loom</a> to record)</p>
         </div>
+    </section>
+
+    <section>
+        <h2>Technical Information</h2>
+
+        <h4>Data privacy and safety</h4>
+        <ol>
+            <li>
+                This web form does not send any data to a server.
+                We do not retain or have access to your data.
+                Only you have access to your data as it is stored in your web browser on your computer.
+            </li>
+            <li>
+                Web browsers provide a function to delete browsing data (or “clear site data”).
+                As your data is stored in the browser, deleteing browsing data for this website will lead to the loss of your data.
+            </li>
+        </ol>
+
+        <h4>Limitations and recommendations</h4>
+        <ol>
+            <li>It is necessary to complete the process using the same browser on the same computer.</li>
+            <li>Use a proper large-screen computer to complete the form. This experience is not ideal for mobile screens.</li>
+            <li>Download the PDF on the last step to preserve the results.</li>
+        </ol>
     </section>
 
     <section>
