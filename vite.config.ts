abc2dccb8bb32bbf21a5e845b78005ea76606abb
Eugene Janusov <esycat@gmail.com>
2025-02-26 17:53:58 +1100

    [ENG-46] build(deps): Avoid large chunk size warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
 import { sveltekit }    from "@sveltejs/kit/vite";
 import tailwindcss      from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
         sveltekit(),
     ],
+    build: {
+        chunkSizeWarningLimit: 750,
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes("@tiptap")) return "tiptap";
+                },
+            },
+        },
+    },
 });
